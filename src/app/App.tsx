6840ff3306d344ab19f502b830616a1c6087e912
Nathan <antnefedov33@gmail.com>
2023-12-27 18:33:32 +0300

    wrote Contact section with adaptive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { Advantages } from 'entities/Advantages';
 import { Advice } from 'entities/Advice';
+import { Contact } from 'entities/Contact';
 import { Header } from 'entities/Header';
 import { Logos } from 'entities/Logos';
 import { Solution } from 'entities/Solution';
@@ -22,6 +23,7 @@ const App = () => {
             <Logos />
             <Subscribe />
             <Support />
+            <Contact />
         </div>
     )
 }
---
+++ src/entities/Contact/ui/Contact.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './Contact.module.scss';
+
+export const Contact = memo(() => {
+    return (
+        <section className={classNames(cls.Contact, {}, [])}>
+            <div className={classNames(cls.Contact__container, {}, ['container'])}>
+                <div className={cls.Contact__textWrapper}>
+                    <h3>
+                        Consulting Agency For Your Business
+                    </h3>
+                    <p>
+                        the quick fox jumps over the lazy dog
+                    </p>
+                </div>
+
+                <button>Contact Us</button>
+            </div>
+        </section>
+    );
+});
+
+Contact.displayName = 'Contact';
